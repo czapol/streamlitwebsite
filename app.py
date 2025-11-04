022d59b55a603cb93daada11474d7d4df40725a9
michalcz <czapski.michal@gmail.com>
2025-11-04 12:09:12 -0800

    chore: setting up basic page with a link

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(page_title="Michal Czapski Webpage", page_icon="🧠", layout="wide")
+#mojis at https://www.webfx.com/tools/emoji-cheat-sheet/
+#layout="wide" - use entire section 
+
+
+#---- HEADER SECTION ---
+st.subheader("Welcome")
+st.title("From messy metrics to confident calls. 🧠")
+st.write("I help product & data teams align on the real problem, define metrics that matter, and make better bets—without burning out.")
+st.write("[Learn More](https://www.linkedin.com/in/michal-czapski/)")
